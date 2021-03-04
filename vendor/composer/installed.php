4dd1f4164e6cfd5671e3d1c7b3542049fbd9e5c3
Vogusov <vogusov@gmail.com>
2021-03-04 16:43:43 +0300

    namespaces added, classnames modified, composer autoload implemented, controller namespace added to config

---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f084bc80d5cde272c3ee3ca0b7891e7b6a8e1e99',
+    'dev-requirement' => true,
+    'name' => 'vendor_name/framework2',
+  ),
+  'versions' => 
+  array (
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+      'dev-requirement' => false,
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+      'dev-requirement' => false,
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1f3b7e2c06cc05d42936a8ad508ff1db7975cdc5',
+      'dev-requirement' => false,
+    ),
+    'vendor_name/framework2' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f084bc80d5cde272c3ee3ca0b7891e7b6a8e1e99',
+      'dev-requirement' => false,
+    ),
+  ),
+);
