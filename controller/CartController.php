4f3dbd22c586ec4173fee2905b599135a3cb19b2
Vogusov <vogusov@gmail.com>
2021-03-31 18:05:15 +0300

    trying ajax. jquery.

--- controller/CartController.php
+++ controller/CartController.php
@@ -17,12 +17,32 @@ class CartController extends Controller
    * */
   function index($data)
   {
-    return [
-      'sitename' => $this->sitename,
-      'content_data' => "Это ваша корзина! Она пока пуста. Дождитесь, пока появятся товары в нашем <a href=\"/index.php?path=catalog/\">магазине</a>",
-      'title' => $this->title,
-      'view' => $this->view
-    ];
+    if (isset($_POST['action'])){
+      // делаем action
+      self::add($_POST['id']);
+//      echo '!Data from PHP!';
+    } else {
+      // возвращаем индексную страницу
+      return [
+        'sitename' => $this->sitename,
+        'content_data' => "Это ваша корзина! Она пока пуста. Дождитесь, пока появятся товары в нашем <a href=\"/index.php?path=catalog/\">магазине</a>",
+        'title' => $this->title,
+        'view' => $this->view
+      ];
+
+    }
+  }
+
+  protected static function add($id) {
+
+    if (!empty($id)){
+      echo "ID of Prod: $id;";
+
+    } else {
+      echo "Data [ID] is empty!!!";
+    }
+
+//    return "есть контакт!";
   }
 }
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,59 @@
 $(document).ready(function(){
-   $('#buyme').on('click', function(){
-       var id_good = $(this).attr("class").substr(5);
 
-       $.ajax({
-           url: "/order/add/",
-           type: "POST",
-           data:{
-               id_good: id_good,
-               quantity: 1
-           },
-           error: function() {alert("Что-то пошло не так...");},
-           success: function(answer){
-               alert("Товар добавлен в корзину!");
-           },
-           dataType : "json"
-       })
-   });
-});
+
+//------ AJAX queries -------------
+// запрос на добавление в корзину
+    btn = $('.js-add-to-cart')
+
+    btn.click(function () {
+        productId = $(this).data('id')
+        productName = $(this).data('name')
+        console.log('prod: ', `${productId} - ${productName};`)
+
+        $.ajax({
+            type: 'POST',
+            url: '/index.php?path=cart/' + productId,
+            data:
+              {
+                  action: 'add',
+                  id: productId
+              },
+            error: function() {alert("Что-то пошло не так...");},
+            success: function (data) {
+                if (data) {
+                    console.log('data: ' + data)
+                    alert('Товар ' + productName + ' добавлен в корзину')
+
+                    // Тут посчитать сумму товаров.
+                    // countTotalSumInCart()
+                }
+            }
+        })
+    })
+
+
+//---the end;
+})
+
+
+
+
+// $(document).ready(function(){
+//    $('#js-add-to-cart').on('click', function(){
+//        var id_good = $(this).attr("class").substr(5);
+//
+//        $.ajax({
+//            url: "/order/add/",
+//            type: "POST",
+//            data:{
+//                id_good: id_good,
+//                quantity: 1
+//            },
+//            error: function() {alert("Что-то пошло не так...");},
+//            success: function(answer){
+//                alert("Товар добавлен в корзину!");
+//            },
+//            dataType : "json"
+//        })
+//    });
+// });
