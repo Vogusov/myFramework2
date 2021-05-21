6913b92b0c03222793bd7c75bb1b557c4235b143
Vogusov <vogusov@gmail.com>
2021-05-21 13:39:42 +0300

    succes message after signup

--- controller/UserController.php
+++ controller/UserController.php
@@ -79,28 +79,37 @@ class UserController extends Controller
       }
       // Если нет поста, то открываем страницу входа!
     } else {
-      $result = [
-        'sitename' => $this->sitename,
-        'content_data' => [
-          'message' => 'Войдите или зарегистрируйтесь.',
-        ],
-        'title' => 'Вход',
-        'view' => $this->view
-      ];
-      if ($data['success']) {
-        $result['content_data']['message'] = 'Вы успешно зарегистрировались, войдите под своим именем!';
-      }
-      return $result;
-    }
 
+      $message = 'Войдите или зарегистрируйтесь.';
+      if (strpos($_SERVER['HTTP_REFERER'], '?path=user/signuppage') > 0 && isset($_COOKIE['successMessage'])) {
+        $message = $_COOKIE['successMessage'];
+        unset($_COOKIE['successMessage']);
+        setcookie('successMessage', 'Поздравляем! Вы успешно зарегистрировались!', time() - 3600, '/');
+        print_r($_COOKIE['successMessage']);
+      }
+
+    $result = [
+      'sitename' => $this->sitename,
+      'content_data' => [
+        'message' => $message,
+      ],
+      'title' => 'Вход',
+      'view' => $this->view
+    ];
+    if ($data['success']) {
+      $result['content_data']['message'] = 'Вы успешно зарегистрировались, войдите под своим именем!';
+    }
+    return $result;
   }
 
+}
 
-  public function logout()
-  {
-    if ($this->user->unsetLoggedSession()) {
-      $_SESSION['asAjax'] = true;
-      return true;
+
+public function logout()
+{
+  if ($this->user->unsetLoggedSession()) {
+    $_SESSION['asAjax'] = true;
+    return true;
 //        [
 //        'sitename' => $this->sitename,
 //        'view' => 'index/index.html',
@@ -108,9 +117,9 @@ class UserController extends Controller
 //          'message' => "Вы вышли из аккаунта! ",
 //        ],
 //      ];
-    } else {
-      $_SESSION['asAjax'] = true;
-      return false;
+  } else {
+    $_SESSION['asAjax'] = true;
+    return false;
 //        [
 //        'sitename' => $this->sitename,
 //        'view' => 'index/index.html',
@@ -118,122 +127,81 @@ class UserController extends Controller
 //          'message' => "Вы НЕ вышли из аккаунта! ",
 //        ]
 //      ];
-    }
   }
+}
 
 
-  public function account()
-  {
-    if (isset($_SESSION['logged_user'])) {
-      $id = $_SESSION['logged_user'];
-      $userData = $this->user->getData($id);
+public
+function account()
+{
+  if (isset($_SESSION['logged_user'])) {
+    $id = $_SESSION['logged_user'];
+    $userData = $this->user->getData($id);
 //      print_r($userData);
-      $userName = $userData['name'];
-      $userEmail = $userData['email'];
-      $userPhone = $userData['phone'];
-      $userLogin = $userData['login'];
-      return [
-        'sitename' => $this->sitename,
-        'view' => 'user/account.html',
-        'title' => 'Личный кабинет',
-        'content_data' => [
-          'user' => [
-            'name' => $userName,
-            'email' => $userEmail,
-            'phone' => $userPhone,
-            'login' => $userLogin,
-          ]
-        ],
-      ];
-    } else {
-      return $this->index();
-    }
-
-  }
-
-
-  /**
-   * Функция устарела
-   * Регистрация нового пользователя
-   *
-   * @param array $data
-   * @return array ['sitename', 'content_data', 'title', 'view']
-   * */
-//  public function signup1()
-//  {
-//    // регистрация нового пользователя
-//    if (isset($_POST['reg'])) {
-//      $data = $_POST;
-//      $user = new User();
-//
-//      if ($result = $user->registrate($data)) {
-//
-//        if ($result['success']) {
-//          echo 'Есть результат! ';
-//          print_r($result);
-//          return $this->login($result);
-//
-//        } else {
-//          return [
-//            'sitename' => $this->sitename,
-//            'content_data' => [
-//              'message' => array_shift($result),
-//              'login' => $_POST['login'],
-//              'name' => $_POST['name'],
-//              'email' => $_POST['email'],
-//              'phone' => $_POST['phone'],
-//            ],
-//            'title' => 'Регистрация',
-//            'view' => 'user/registration.html'
-//          ];
-//        }
-//      }
-//
-//    } // если нет поста, отображается страница регистрации по умолчанию
-//    else {
-//      return [
-//        'sitename' => $this->sitename,
-//        'content_data' => [
-//          'message' => 'Зарегистрируйтесь на нашем сайте. Пожалуйста: ',
-//        ],
-//        'title' => 'Регистрация',
-//        'view' => 'user/registration.html'
-//      ];
-//    }
-//  }
-
-  /**
-   * открывает страницу регистрации
-   *
-   * @return array
-   */
-  public function signupPage()
-  {
+    $userName = $userData['name'];
+    $userEmail = $userData['email'];
+    $userPhone = $userData['phone'];
+    $userLogin = $userData['login'];
     return [
       'sitename' => $this->sitename,
+      'view' => 'user/account.html',
+      'title' => 'Личный кабинет',
       'content_data' => [
-        'message' => 'Зарегистрируйтесь на нашем сайте. Пожалуйста: ',
+        'user' => [
+          'name' => $userName,
+          'email' => $userEmail,
+          'phone' => $userPhone,
+          'login' => $userLogin,
+        ]
       ],
-      'title' => 'Регистрация',
-      'view' => 'user/registration.html'
     ];
+  } else {
+    return $this->index();
   }
 
-  /**
-   * принимает и обрабатывает форму регистрации пользователя
-   */
-  public function signup()
-  {
-    $_SESSION['asAjax'] = true;
-    if (!$_POST) {
-      die('Нет данных нового пользователя для обработки');
-    }
+}
 
-    $json = file_get_contents('php://input');
-    $data = json_decode($json, true);
 
-    $user = new User();
-    return json_encode($user->registrate($data));
+/**
+ * открывает страницу регистрации
+ *
+ * @return array
+ */
+public
+function signupPage()
+{
+  return [
+    'sitename' => $this->sitename,
+    'content_data' => [
+      'message' => 'Зарегистрируйтесь на нашем сайте. Пожалуйста: ',
+    ],
+    'title' => 'Регистрация',
+    'view' => 'user/registration.html'
+  ];
+}
+
+/**
+ * Принимает и обрабатывает форму регистрации пользователя
+ *
+ * @return string Возврощает json с резулбтатом регистрации клиенту
+ */
+public
+function signup()
+{
+  $_SESSION['asAjax'] = true;
+  if (!$_POST) {
+    die('Нет данных нового пользователя для обработки');
   }
+
+  $json = file_get_contents('php://input');
+  $data = json_decode($json, true);
+
+  $user = new User();
+  $result = $user->registrate($data);
+  if ($result['success']) {
+    setcookie('successMessage', 'Поздравляем! Вы успешно зарегистрировались!', time() + 3600, '/');
+  }
+  return json_encode($result);
+}
 
 }
